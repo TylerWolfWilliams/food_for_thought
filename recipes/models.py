051556355c29cfa8fbe5663e314c21156cdc0512
M4G13 <msandreereid@protonmail.com>
2023-02-24 20:52:40 +0000

    intial models work

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    title = models.CharField(max_length = 100)
+    image = models.ImageField()
+    content = models.CharField(max_length = 10000)
+    ingredients = models.CharField(max_length = 1000)
+    cooking_time = models.TimeField()
+    serving = models.CharField(max_length = 100)
+    tags = models.CharField(max_length = 1000)
+
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    saved = models.ManyToManyField(Recipe, related_name="saved")
+
+    picture = models.ImageField()
+    bio = models.CharField(max_length = 1000)
+
+    def __str__(self):
+        return self.user.username
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    content = models.CharField(max_length = 1000)
+    rating = models.FloatField()
