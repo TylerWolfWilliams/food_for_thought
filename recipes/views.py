8c1b77fc84933b37108d345d73728cc01e517821
Kirsty Balfour <2635375b@student.gla.ac.uk>
2023-03-05 22:08:26 +0000

    Initial Views Work

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from food_for_thought import settings
+from food_for_thought.models import Category, Recipe
+
 
 # Create your views here.
+
+def home(request):
+    category_list = Category.objects.all()[:5]
+    recipe_list = Recipe.objects.order_by('-average_rating')[:5]
+
+    context_dict = {'categories': category_list, 'recipes': recipe_list}
+
+    return render(request, 'food_for_thought/home.html', context=context_dict)
+
+def categories(request):
+    category_list = Category.objects.all()
+
+    context_dict = {'categories': category_list}
+
+    return render(request, 'food_for_thought/categories.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+
+
