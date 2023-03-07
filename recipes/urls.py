a07ff66e729a7352c824ad71aaff5c81d48cdb64
Tyler Wolf Williams <2869420W@student.gla.ac.uk>
2023-03-07 14:24:00 +0000

    Trying to run the site

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,10 +6,10 @@ app_name = "food_for_thought"
 urlpatterns = [
     path('', views.home, name='home'),
     path('categories/', views.categories, name='categories'),
-    path('category/<name:category_name>/', views.show_category, name='show_category'),
+    path('category/<slug:category_name>/', views.show_category, name='show_category'),
     path('results/', views.show_results, name='show_results'),
     path('login/', views.user_login, name='login'),
     path('signup/', views.sign_up, name='signup'),
-    path('<slug:recipe_name_slug>/<id:recipe_id>/', views.show_recipe, name='show_recipe'),
-    path('myaccount/addrecipe/', views.add_recipe, name='add_recipe'),
+    path('<slug:recipe_name_slug>/<int:recipe_id>/', views.show_recipe, name='show_recipe'),
+    #path('myaccount/addrecipe/', views.add_recipe, name='add_recipe'),
 ]
