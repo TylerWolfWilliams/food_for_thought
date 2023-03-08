d406479e2fd26e7678420cfad89178843a2b00b8
Anzela Tariq <2665724t@student.gla.ac.uk>
2023-03-08 19:26:56 +0000

    Drop down for categories

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
-from recipes.models import Recipe, UserProfile, Review
+from recipes.models import Recipe, UserProfile, Review, Category
+
+category_list = Category.objects.all()
+CATEGORIES = []
+for category in category_list:
+    CATEGORIES.append((category, category.slug))
 
 class RecipeForm(forms.ModelForm):
     title = forms.CharField(max_length=100, help_text="Please enter the name of the recipe.")
@@ -8,12 +13,14 @@ class RecipeForm(forms.ModelForm):
     content = forms.CharField(max_length=10000, help_text="Please enter instructions for this recipe.")
     ingredients = forms.CharField(max_length=10000, help_text="Please enter the ingredients for the recipe.")
     tags = forms.CharField(max_length=1000, help_text="Please enter tags")
-    cooking_time = forms.IntegerField()
+    cooking_time = forms.DurationField()
     servings = forms.CharField(max_length=100, help_text="How many servings?")
+
+    category = forms.CharField(max_length=100, label="Category: ", widget=forms.Select(choices=CATEGORIES))
 
     class Meta:
         model = Recipe
-        exclude = ('author', 'category',)
+        exclude = ('author',)
 
 
 class UserForm(forms.ModelForm):
